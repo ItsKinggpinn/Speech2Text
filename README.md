cea078d43686fb8cf5d7a5619db748dfa294951c
Soumyajit Nag <97018860+ItsKinggpinn@users.noreply.github.com>
2023-07-14 02:57:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Text2Speech
+
+This is a Speech to Text Converter (Text to Speech's continuation - Reverse project) made with HTML, CSS, JavaScript and Tailwind CSS that simply runs on Web Speech API.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Tailwind CSS
+- Web Speech API
+
+## Credits
+
+- [Tailwind CSS](https://tailwindcss.com/)
+- [Open Source Coding](https://www.youtube.com/watch?v=iMwEYy-ETos)
---
+++ languages.js
@@ -0,0 +1,62 @@
+const languages = [
+    {
+      no: "16",
+      name: "English",
+      native: "English",
+      code: "en",
+    },
+    {
+      no: "3",
+      name: "Arabic",
+      native: "عربي",
+      code: "ar",
+    },
+    {
+      no: "20",
+      name: "French",
+      native: "Français",
+      code: "fr",
+    },
+    {
+      no: "23",
+      name: "German",
+      native: "Deutsch",
+      code: "de",
+    },
+    {
+      no: "27",
+      name: "Hindi",
+      native: "हिन्दी",
+      code: "hi",
+    },
+    {
+      no: "31",
+      name: "Irish",
+      native: "Gaeilge",
+      code: "ga",
+    },
+    {
+      no: "32",
+      name: "Italian",
+      native: "Italiano",
+      code: "it",
+    },
+    {
+      no: "33",
+      name: "Japanese",
+      native: "日本語",
+      code: "ja",
+    },
+    {
+      no: "34",
+      name: "Korean",
+      native: "한국어",
+      code: "ko",
+    },
+    {
+      no: "49",
+      name: "Spanish",
+      native: "Español",
+      code: "es",
+    },
+  ];
